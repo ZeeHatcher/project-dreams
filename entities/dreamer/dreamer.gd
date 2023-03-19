8d71c84ff3aef8bb023a7924194eed2b5125b0d5
ZeeHatcher <zee.hatcher010@gmail.com>
2023-03-20 01:21:40 +0800

    show minigame result in the form of dreamer emoticons

--- entities/dreamer/dreamer.gd
+++ entities/dreamer/dreamer.gd
@@ -3,8 +3,13 @@ extends StaticBody2D
 
 signal dream_entered(minigame)
 
+const Happy = preload("res://entities/dreamer/happy.png")
+const Sad = preload("res://entities/dreamer/sad.png")
+
 export(Globals.Minigames) var minigame
 export(PackedScene) var minigame_scene
+
+var _completed = false
 
 
 func _ready():
@@ -13,16 +18,21 @@ func _ready():
 	if not minigame in MapData.minigames_result:
 		MapData.minigames_result[minigame] = 0
 	elif MapData.minigames_result[minigame] != 0:
-		set_collision_layer_bit(0, false)
+#		set_collision_layer_bit(0, false)
+		_completed = true
+		$Emotion.texture = Happy if MapData.minigames_result[minigame] == 1 else Sad
 
 
 func interact():
+	if _completed:
+		return
+	
 	emit_signal("dream_entered", minigame_scene)
 
 
 func highlight():
-	print_debug("'highlight' called on '%s'" % self)
+	create_tween().tween_property($Emotion, "modulate", Color.white, 0.5)
 
 
 func unhighlight():
-	print_debug("'unhighlight' called on '%s'" % self)
+	create_tween().tween_property($Emotion, "modulate", Color.transparent, 0.5)
